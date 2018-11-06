bb88d193d4508b8c09088d946c94f7ed6b0ea423
Kalli <pturon@fh-bielefeld.de>
2018-11-06 16:49:02 +0100

    added pull down refresh

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   constructor(public navCtrl: NavController) {
-
   }
+
+  ngOnInit(): void {
+  }
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+
 
 }
