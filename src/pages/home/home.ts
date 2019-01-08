dd780bcdd7c6adbb8ff87e0498bee2335ade165f
Kalli <pturon@fh-bielefeld.de>
2019-01-08 11:25:00 +0100

    removed unused code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,10 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, ActionSheetController, AlertController} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {BLE} from '@ionic-native/ble';
 import {Base64} from '@ionic-native/base64';
 import {FileChooser} from '@ionic-native/file-chooser';
-import { FilePath } from '@ionic-native/file-path';
+import {FilePath} from '@ionic-native/file-path';
 
 @Component({
   selector: 'page-home',
@@ -12,15 +12,9 @@ import { FilePath } from '@ionic-native/file-path';
   providers: [BLE, Base64, FileChooser, FilePath]
 })
 
-export class HomePage implements OnInit {
+export class HomePage {
+
   devices: Array<any>;
-
-  lines: string[];
-
-  newLine = {
-    line: 0,
-    text: ''
-  }
 
   device = {
     'name': '',
@@ -38,35 +32,21 @@ export class HomePage implements OnInit {
 
   imageURL = "";
 
-  /**
-   @ViewChild('canvas') canvasEl: ElementRef;
-   private _CANVAS: any;
-   private _CONTEXT: any;
-   **/
-
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController,
               public  ble: BLE, private statusBar: StatusBar, private base64: Base64, private fileChooser: FileChooser, private filePath: FilePath) {
   }
 
-  ngOnInit(): void {
+  ionViewDidLoad() {
 
-    this.ionViewDidLoad();
-    //console.log(this.base64.encodeFile("src/katze_2.jpg"));
+    this.statusBar.overlaysWebView(true);
+    this.statusBar.styleBlackTranslucent();
+    this.statusBar.backgroundColorByHexString('#ffffff');
 
-  }
-
-  onDeviceDiscovered(device) {
-    //console.log(device);
-    if (device.name != null) {
-      this.devices.push(device);
-    }
-  }
-
-  initLines() {
-    this.lines = Array(5);
-    for (let i = 0; i < this.lines.length; i++) {
-      this.lines[i] = "";
-    }
+    this.devices = new Array<any>(0);
+    this.ble.scan([], 2).subscribe(
+      device => this.onDeviceDiscovered(device),
+      error => this.showBluetoothError()
+    );
   }
 
   doRefresh(refresher) {
@@ -79,6 +59,100 @@ export class HomePage implements OnInit {
       refresher.complete();
     }, 2000);
   }
+
+  onDeviceDiscovered(device) {
+    //console.log(device);
+    if (device.name != null) {
+      this.devices.push(device);
+    }
+  }
+
+  showConnectDeviceDialog(device) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: device.name,
+      buttons: [
+        {
+          text: 'Verbinden',
+          handler: () => {
+            this.connect(device);
+          }
+        },
+        {
+          text: 'Trennen',
+          handler: () => {
+            this.disconnect(device);
+          }
+
+        },
+        {
+          text: 'Abbrechen',
+          role: 'cancel',
+          handler: () => {
+
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
+  connect(device) {
+    if (this.connected) {
+      this.ble.connect(device.id).subscribe(
+        peripheral => this.onConnected(peripheral),
+        error => this.showConnectError()
+      );
+    }
+  }
+
+  onConnected(peripheral) {
+    this.device = peripheral;
+    console.log("Verbunden");
+  }
+
+  disconnect(device) {
+    this.ble.disconnect(device.id).then(res => {
+      this.connected = false;
+    }).catch(err => {
+      this.showDisconnectError()
+    })
+  }
+
+  choseImage() {
+    this.fileChooser.open()
+      .then(uri => this.filePath.resolveNativePath(uri)
+        .then(filePath => this.imageURL = filePath)
+        .catch(err => console.log(err)))
+      .catch(e => console.log(e));
+  }
+
+  uploadData() {
+    //if(this.connected){
+    this.base64.encodeFile(this.imageURL).then((base64File: string) => {
+      console.log(base64File);
+      console.log("Test");
+    }, (err) => {
+      console.log(err);
+    });
+    //} else {
+    //  this.showNoConnectionError();
+    //}
+  }
+
+  sendData(data: string) {
+    let index = 0;
+    while (index < data.length) {
+      let sendingData = new Uint8Array(20);
+      for (let i = 0; i < sendingData.length && index < data.length; i++, index++) {
+        sendingData[i] = data.charCodeAt(index);
+      }
+      if (this.connected) {
+        this.ble.write(this.device.id, this.serviceUUID, this.characteristicUUID, sendingData.buffer);
+      }
+    }
+  }
+
 
   showBluetoothError() {
     let alert = this.alertCtrl.create({
@@ -116,149 +190,7 @@ export class HomePage implements OnInit {
     alert.present();
   }
 
-  uploadData() {
-    //if(this.connected){
-    this.base64.encodeFile(this.imageURL).then((base64File: string) => {
-      console.log(base64File);
-      console.log("Test");
-    }, (err) => {
-      console.log(err);
-    });
-    //} else {
-    //  this.showNoConnectionError();
-    //}
-  }
 
-  connect(device) {
-    if (this.connected) {
-      this.ble.connect(device.id).subscribe(
-        peripheral => this.onConnected(peripheral),
-        error => this.showConnectError()
-      );
-    }
-  }
-
-  onConnected(peripheral) {
-    this.device = peripheral;
-    console.log("Verbunden");
-  }
-
-  disconnect(device) {
-    this.ble.disconnect(device.id).then(res => {
-      this.connected = false;
-    }).catch(err => {
-      this.showDisconnectError()
-    })
-  }
-
-  showConnectDeviceDialog(device) {
-    let actionSheet = this.actionSheetCtrl.create({
-      title: device.name,
-      buttons: [
-        {
-          text: 'Verbinden',
-          handler: () => {
-            this.connect(device);
-          }
-        },
-        {
-          text: 'Trennen',
-          handler: () => {
-            this.disconnect(device);
-          }
-
-        },
-        {
-          text: 'Abbrechen',
-          role: 'cancel',
-          handler: () => {
-
-          }
-        }
-      ]
-    });
-
-    actionSheet.present();
-  }
-
-  ionViewDidLoad() {
-
-    this.statusBar.overlaysWebView(true);
-    this.statusBar.styleBlackTranslucent();
-    this.statusBar.backgroundColorByHexString('#ffffff');
-
-    this.devices = new Array<any>(0);
-    this.ble.scan([], 2).subscribe(
-      device => this.onDeviceDiscovered(device),
-      error => this.showBluetoothError()
-    );
-
-    /**
-     this.initLines();
-
-     this._CANVAS = this.canvasEl.nativeElement;
-     this._CANVAS.width = 800;
-     this._CANVAS.height = 600;
-
-     this.initialiseCanvas();
-     **/
-  }
-
-  /**
-   initialiseCanvas() {
-    if (this._CANVAS.getContext) {
-      this.setupCanvas();
-    }
-  }
-
-   setupCanvas() {
-    this._CONTEXT = this._CANVAS.getContext('2d');
-    this._CONTEXT.fillStyle = '#3e3e3e';
-    this._CONTEXT.fillRect(0, 0, 500, 500);
-  }
-
-   clearCanvas() {
-    this._CONTEXT.clearRect(0, 0, this._CANVAS.width, this._CANVAS.height);
-    this.setupCanvas();
-  }
-
-   addLine() {
-    if (this.newLine.line <= this.lines.length) {
-      this.lines[this.newLine.line - 1] = this.newLine.text;
-    }
-    this.drawText();
-  }
-
-   drawText() {
-    this.clearCanvas();
-    this._CONTEXT.fillStyle = '#ffffff';
-    this._CONTEXT.font = '30px Arial';
-
-    for (let i = 0; i < this.lines.length; i++) {
-      this._CONTEXT.fillText(this.lines[i], 10, 50 + (35 * i));
-    }
-  }
-   **/
-  sendData(data: string) {
-    let index = 0;
-    while (index < data.length) {
-      let sendingData = new Uint8Array(20);
-      for (let i = 0; i < sendingData.length && index < data.length; i++, index++) {
-        sendingData[i] = data.charCodeAt(index);
-      }
-      if (this.connected) {
-        this.ble.write(this.device.id, this.serviceUUID, this.characteristicUUID, sendingData.buffer);
-      }
-    }
-  }
-
-  choseImage() {
-    this.fileChooser.open()
-      .then(uri => this.filePath.resolveNativePath(uri)
-        .then(filePath => this.imageURL = filePath)
-        .catch(err => console.log(err)))
-      .catch(e => console.log(e));
-  }
 }
 
 
