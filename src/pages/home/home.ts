336244d93ea2f3e7a822646de2b02141fb5b14d1
Kalli <pturon@fh-bielefeld.de>
2019-01-16 11:18:30 +0100

    color correction

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,28 +2,18 @@ import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NavController, ActionSheetController, AlertController} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {BLE} from '@ionic-native/ble';
-import {Base64} from '@ionic-native/base64';
-import {FileChooser} from '@ionic-native/file-chooser';
-import {FilePath} from '@ionic-native/file-path';
 import {LoadingController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [BLE, Base64, FileChooser, FilePath]
+  providers: [BLE]
 })
 
 export class HomePage {
 
-  /**
-   * Liste aller gefundenen Geräte
-   */
   devices: Array<any>;
 
-  /**
-   * Aktuell verbundenes Gerät
-   * @type {{name: string; id: string; advertising: number[]; rssi: number; services: any[]; characteristics: any[]}}
-   */
   device = {
     'name': '',
     'id': '',
@@ -33,35 +23,14 @@ export class HomePage {
     'characteristics': []
   };
 
-  /**
-   *
-   * @type {boolean}
-   */
   connected = false;
 
-  /**
-   * UUID des Services
-   * @type {string}
-   */
   serviceUUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 
-  /**
-   * UUID der zu ändernen Characteristik
-   * @type {string}
-   */
-  characteristicUUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+  sendUUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+  statusUUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
 
-  /**
-   * Pfad zum ausgewähltem Bild
-   * @type {string}
-   */
-  imageURL = "";
-
-  /**
-   * Base64 String des Bildes
-   * @type {string}
-   */
-  dataURL = "";
+  deviceStatus = true;
 
   lines: string[];
 
@@ -71,36 +40,23 @@ export class HomePage {
   }
 
   saveX: number;
-  saveY: number
+  saveY: number;
 
   selectedColor = '#000000';
 
-  colors = [ '#eeeeee', '#444444', '#000000'];
+  backgroundColor = '#ffffff';
+
+  colors = ['#ffffff', '#C0C0C0', '#808080', '#000000'];
 
   @ViewChild('canvas') canvasEl: ElementRef;
-  private _CANVAS: any;
-  private _CONTEXT: any;
+  private canvas: any;
+  private context: any;
 
-  /**
-   * Konstruktor der Klasse
-   * @param {NavController} navCtrl
-   * @param {AlertController} alertCtrl Controller für Popups
-   * @param {ActionSheetController} actionSheetCtrl Controller für Auswahlfenster
-   * @param {BLE} ble Natives Bluetooth LE Plugin
-   * @param {StatusBar} statusBar Native StatusBar
-   * @param {Base64} base64 Base64 Encoder
-   * @param {FileChooser} fileChooser Nativer Datenexplorer
-   * @param {FilePath} filePath
-   */
+
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController,
-              public  ble: BLE, private statusBar: StatusBar, private base64: Base64, private fileChooser: FileChooser,
-              private filePath: FilePath, private loading: LoadingController) {
+              public  ble: BLE, private statusBar: StatusBar, private loading: LoadingController) {
   }
 
-  /**
-   * Wird beim Start der App ausgeführt.
-   * Initialisiert StatusBar und Geräteliste
-   */
   ionViewDidLoad() {
 
     this.statusBar.overlaysWebView(true);
@@ -115,9 +71,9 @@ export class HomePage {
 
     this.initLines();
 
-    this._CANVAS = this.canvasEl.nativeElement;
-    this._CANVAS.width = 400;
-    this._CANVAS.height = 300;
+    this.canvas = this.canvasEl.nativeElement;
+    this.canvas.width = 400;
+    this.canvas.height = 300;
 
     this.initialiseCanvas();
   }
@@ -130,15 +86,15 @@ export class HomePage {
   }
 
   initialiseCanvas() {
-    if (this._CANVAS.getContext) {
-      this._CONTEXT = this._CANVAS.getContext('2d');
-      this._CONTEXT.fillStyle = '#ffffff';
-      this._CONTEXT.fillRect(0, 0, 400, 300);
+    if (this.canvas.getContext) {
+      this.context = this.canvas.getContext('2d');
+      this.context.fillStyle = this.backgroundColor;
+      this.context.fillRect(0, 0, 400, 300);
     }
   }
 
   clearCanvas() {
-    this._CONTEXT.clearRect(0, 0, this._CANVAS.width, this._CANVAS.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.initialiseCanvas();
   }
 
@@ -151,11 +107,11 @@ export class HomePage {
 
   drawText() {
     this.clearCanvas();
-    this._CONTEXT.fillStyle = '#000000';
-    this._CONTEXT.font = '30px Arial';
+    this.context.fillStyle = this.selectedColor;
+    this.context.font = '30px Arial';
 
     for (let i = 0; i < this.lines.length; i++) {
-      this._CONTEXT.fillText(this.lines[i], 5, 10 + (35 * i));
+      this.context.fillText(this.lines[i], 5, 10 + (35 * i));
     }
   }
 
@@ -163,17 +119,22 @@ export class HomePage {
     this.selectedColor = color;
   }
 
+  selectBackground(color) {
+    this.backgroundColor = color;
+    this.clearCanvas();
+  }
+
   startDrawing(ev) {
-    var canvasPosition = this._CANVAS.getBoundingClientRect();
+    var canvasPosition = this.canvas.getBoundingClientRect();
 
     this.saveX = ev.touches[0].pageX - canvasPosition.x;
     this.saveY = ev.touches[0].pageY - canvasPosition.y;
   }
 
   moved(ev) {
-    var canvasPosition = this._CANVAS.getBoundingClientRect();
+    var canvasPosition = this.canvas.getBoundingClientRect();
 
-    let ctx = this._CANVAS.getContext('2d');
+    let ctx = this.canvas.getContext('2d');
     let currentX = ev.touches[0].pageX - canvasPosition.x;
     let currentY = ev.touches[0].pageY - canvasPosition.y;
 
@@ -192,10 +153,6 @@ export class HomePage {
     this.saveY = currentY;
   }
 
-  /**
-   * Scannt nach Bluetoothgeräten in der Umgebung
-   * @param refresher Objekt im Frontend
-   */
   doRefresh(refresher) {
     this.devices = new Array<any>(0);
     this.ble.scan([], 2).subscribe(
@@ -207,20 +164,12 @@ export class HomePage {
     }, 2000);
   }
 
-  /**
-   * Fügt ein Gerät zur Liste hinzu, wenn es beim scannen entdeckt wurde.
-   * @param device Entdecktes Gerät
-   */
   onDeviceDiscovered(device) {
     if (device.name != null) {
       this.devices.push(device);
     }
   }
 
-  /**
-   * Öffnet einen Dialog um sich mit Gerät zu verbinden oder Vernindung zu trennen.
-   * @param device Ausgewähltes Gerät
-   */
   showConnectDeviceDialog(device) {
     let actionSheet = this.actionSheetCtrl.create({
       title: device.name,
@@ -251,23 +200,14 @@ export class HomePage {
     actionSheet.present();
   }
 
-  /**
-   * Verbindet sich mit dem angegebenen Gerät
-   * @param device Gerät mitdem sich verbunden werden soll
-   */
   connect(device) {
     console.log(device.id);
     this.ble.connect(device.id).subscribe(
       peripheral => this.onConnected(peripheral),
       error => console.log(error)
     );
-
   }
 
-  /**
-   * Speichert das verbundene Gerät zwischen
-   * @param peripheral Verbundene Gerät
-   */
   onConnected(peripheral) {
     console.log("verbunden");
     //console.log(peripheral);
@@ -291,72 +231,72 @@ export class HomePage {
     })
   }
 
-  /**
-   * Öffnet die Bildergalerie und Speichert den Pfad des ausgewählten Bildes
-   */
-  choseImage() {
-    this.fileChooser.open()
-      .then(uri => this.filePath.resolveNativePath(uri)
-        .then(filePath => this.imageURL = filePath)
-        .catch(err => console.log(err)))
-      .catch(e => console.log(e));
-  }
-
-  /**
-   *
-   */
-  base64ToByteArray(ble: BLE, device, serviceUUID: string, characteristicUUID: string) {
-    //console.log(this.device);
-    //if (this.connected) {
-    let data = new Uint8Array(2);
-    this.base64.encodeFile(this.imageURL).then((base64File: string) => {
-      this.dataURL = base64File;
-      fetch(this.dataURL.substring(0)).then(function (response) {
-        return response.arrayBuffer()
-      }).then(function (buffer) {
-
-      })
-
-    }, (err) => {
-      console.log(err);
-    });
-    //} else {
-    //this.showNoConnectionError();
-    //}
-    console.log(data);
-  }
-
-  sendTest() {
-    let imageData = this._CONTEXT.getImageData(0, 0, 400, 300);
+  sendImage() {
+    let imageData = this.context.getImageData(0, 0, 400, 300);
     let data = imageData.data;
-    console.log(data);
-    for (let i = 0; i < data.length; i = i + 4) {
+    let error = false;
+    let i = 0;
+    let t = 0;
+    while(i < data.length){
       let sendingData = "";
-      for (let k = 0; k < 20; k++, i += 4) {
+      for (let j = 0; j < 20 && i < data.length; i = i + 4, j++) {
+        t++;
         let block = new Uint8Array(4);
         for (let k = 0; k < 4; k++) {
           let gray = ((data[i] + data[i + 1] + data[i + 2]) / 3);
-          console.log(gray);
-          console.log(data[i + 1]);
-          if (gray > 64) {
-            block[k] = 3;
-          } else if (gray > 128) {
-            block[k] = 2;
-          } else if (gray > 192) {
-            block[k] = 1;
-          } else {
+          if (gray == 0) {
+            imageData.data[i] = 0;
+            imageData.data[i+1] = 0;
+            imageData.data[i+2] = 0;
             block[k] = 0;
+          } else if (gray == 128) {
+            imageData.data[i] = 128;
+            imageData.data[i+1] = 128;
+            imageData.data[i+2] = 128;
+            block[k] = 1;
+          } else if (gray == 192) {
+            imageData.data[i] = 192;
+            imageData.data[i+1] = 192;
+            imageData.data[i+2] = 192;
+            block[k] = 2;
+          } else if(gray == 255){
+            imageData.data[i] = 255;
+            imageData.data[i+1] = 255;
+            imageData.data[i+2] = 255;
+            block[k] = 3;
+          } else {
+            imageData.data[i] = 0;
+            imageData.data[i+1] = 0;
+            imageData.data[i+2] = 0;
+            block[k] = 3  ;
           }
         }
-        let pixelBlock = (block[0] + block[1] * 4 + block[2] * 16 + block[3] * 64).toString();
+        let pixelBlock = (block[0] + (block[1] * 4) + (block[2] * 16) + (block[3] * 64)).toString();
         while (pixelBlock.length < 3) {
           pixelBlock = 0 + "" + pixelBlock;
         }
         sendingData += pixelBlock;
       }
-      this.ble.write(this.device.id, this.serviceUUID, this.characteristicUUID, this.stringToBytes(sendingData));
+
+      this.ble.write(this.device.id, this.serviceUUID, this.sendUUID, this.stringToBytes(sendingData)).catch(err => {
+        let alert = this.alertCtrl.create({
+          title: 'Fehler beim Übertragen',
+          subTitle: err,
+          buttons: ['Ok']
+        });
+        alert.present();
+        error = true;
+      });
+
+      let currentTime = new Date().getTime();
+      while (currentTime + 500 >= new Date().getTime()) {
+      }
     }
+    //imageData.data = data;
+    this.context.putImageData(imageData, 0, 0);
+    console.log(t);
   }
+
 
   onUpload() {
     const loader = this.loading.create({
@@ -364,17 +304,11 @@ export class HomePage {
     });
     loader.present();
 
-    this.sendTest();
+    this.sendImage();
 
     loader.dismiss();
   }
 
-
-  /**
-   * Übersetzt einen String in ein Byte Array
-   * @param string Zu Übersetzene String
-   * @returns String als ArrayBuffer
-   */
   stringToBytes(string) {
     let array = new Uint8Array(string.length);
     for (let i = 0, l = string.length; i < l; i++) {
@@ -383,20 +317,10 @@ export class HomePage {
     return array.buffer;
   }
 
-
   showBluetoothError() {
     let alert = this.alertCtrl.create({
       title: 'Fehler beim Scannen',
       subTitle: 'Beim Scannen ist ein Fehler aufgetreten!',
-      buttons: ['Ok.']
-    });
-    alert.present();
-  }
-
-  showConnectError() {
-    let alert = this.alertCtrl.create({
-      title: 'Fehler bei der Verbindung',
-      subTitle: 'Es konnte sich nicht mit dem Gerät verbunden werden!',
       buttons: ['Ok.']
     });
     alert.present();
